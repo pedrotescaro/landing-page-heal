6894119a562cf9253f42c09bcfe3a706af974c93
Guilherme Alves <guigui21alves@gmail.com>
2025-06-08 14:53:54 -0300

    [feat] Função Mostrar/Esconder senha no Login de usuário

--- js/script-geral.js
+++ js/script-geral.js
@@ -20,3 +20,17 @@ function decreaseFontSize(  ) {
         document.body.style.fontSize = fontSize + 'px';
     }
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const togglePassword = document.getElementById("togglePassword");
+  const password = document.getElementById("password");
+
+  if (togglePassword && password) {
+    togglePassword.addEventListener("click", () => {
+      const type = password.getAttribute("type") === "password" ? "text" : "password";
+      password.setAttribute("type", type);
+      togglePassword.classList.toggle("fa-eye");
+      togglePassword.classList.toggle("fa-eye-slash");
+    });
+  }
+});
